7d9c19bdde755c40344da7281d960278b8534989
Sara Mills <sara.mills@schoolofmotion.com>
2022-07-12 12:41:22 -0400

    completed section 8

---
+++ pages/api/feedback/[feedbackId].js
@@ -0,0 +1,13 @@
+import { buildFeedbackPath, extractFeedback } from ".";
+
+function handler(req, res) {
+  const { feedbackId } = req.query;
+  const filePath = buildFeedbackPath();
+
+  const data = extractFeedback(filePath);
+
+  const selectedFeedback = data.find((item) => item.id === feedbackId);
+
+  res.status(200).json({ feedback: selectedFeedback });
+}
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
+import { useRef, useState } from "react";
+
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+    const email = emailInputRef.current.value;
+    const feedback = feedbackInputRef.current.value;
+
+    const body = { email, feedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log({ data }));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback Address</label>
+          <textarea
+            type="feedback"
+            id="email"
+            rows="5"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
     </div>
   );
 }
